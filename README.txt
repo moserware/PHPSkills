110463ad49670d32d1a8edfbe33126c4569e58a5
Jeff Moser <jeff@moserware.com>
2010-10-09 10:42:00 -0400

    Deleted C# code (it was just there to note the fork)

--- README.txt
+++
@@ -1,4 +0,0 @@
-The code for the TrueSkill calculator is in the "Skills" folder and its unit
-tests are in the "UnitTests" folder.
-
-For more details, see the "README" file in each of those folders.
--- Skills/FactorGraphs/FactorGraph.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Moserware.Skills.FactorGraphs
-{
-    public class FactorGraph<TSelf, TValue, TVariable>
-        where TSelf : FactorGraph<TSelf, TValue, TVariable>
-        where TVariable : Variable<TValue>
-    {
-        public VariableFactory<TValue> VariableFactory { get; protected set; }
-    }
-}
